18ed58af3d610b1d632f1135d86da50d81372e1e
Site13 Soluções Digitais <tulio.nasc95@gmail.com>
2025-07-14 04:05:07 -0300

    Ajustes para deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
-    }
-  }
+    },
+    port: 7000,
+    host: true
+  },
 })
